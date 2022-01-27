a6cd90746d9aaf6eb0e0a56e9556de2f7ef60cb0
fab1opinto <fabiopintoapt@gmail.com>
2022-01-27 12:17:05 -0300

    editando o javascript

--- meujavascript.js
+++ meujavascript.js
@@ -9,7 +9,7 @@ function clicar() {
     var a = window.document.getElementById("meu-elemento").style.borderColor = "red"
     let email = window.document.getElementById("meu-elemento")
     email.value="email@exemplo"
-
+    document.getElementById("Arroba").style.color= "red"
     var res = window.document.getElementById("res")
     res.innerHTML = 'Por favor, insira um email válido'
 }
